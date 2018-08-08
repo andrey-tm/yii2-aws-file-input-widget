472d4b633b842f6d52d69f9cfd8402b3db2ddb94
andreyv <andreyv@templatemonster.me>
2018-08-08 15:01:23 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# yii2-aws-file-input-widget
+Yii2 widget allows to integrate AWS S3 file upload.
+
+## Installation
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require andreyv/yii2-aws-file-input-widget "^1.0"
+```
+
+or add
+
+```
+"andreyv/yii2-aws-file-input-widget": "^1.0"
+```
+
+to the require section of your `composer.json` file.
---
+++ widgets/AwsFileInput.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace andreyv\aws\fileinput\widgets;
+
+use yii\helpers\Inflector;
+use yii\base\InvalidConfigException;
+
+class AwsFileInput extends \kartik\file\FileInput
+{
+    /** @var string AWS component name */
+    public $awsComponent = 'aws';
+
+    /** @var string model unique key */
+    public $uniqueKey = 'id';
+
+    /** @var array Custom filename parts */
+    public $fileNameParts = [];
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        if ($this->options['multiple']) {
+            throw new InvalidConfigException('Widget ' . __CLASS__ . ' - supports only single file uploading');
+        }
+        /** @var \Aws\S3\PostObjectV4 $awsPostObject */
+        $awsPostObject = \Yii::$app->get($this->awsComponent)->createPostObjectV4($this->getUniqueFileName());
+        $this->options['hiddenOptions'] = [
+            'value' => $this->model->{$this->attribute} ?? ''
+        ];
+        $this->defaultPluginOptions['dropZoneEnabled'] = false;
+        $this->pluginOptions = array_merge(
+            $this->pluginOptions,
+            [
+                'uploadAsync' => true,
+                'initialPreview' => $this->model->{$this->attribute} ?? '',
+                'initialCaption'=> $this->model->{$this->attribute} ?? '',
+                'initialPreviewAsData' => true,
+                'uploadUrl' => $awsPostObject->getFormAttributes()['action'],
+                'uploadExtraData' => $awsPostObject->getFormInputs(),
+                'showUpload' => false,
+                'showPreview' => true,
+                'removeClass' => 'btn btn-danger',
+                'layoutTemplates' => [
+                    'footer' => '',
+                    'preview' => join(',', [
+                        '<div class="file-preview {class}">' .
+                        '<div class="file-preview-thumbnails"></div>' .
+                        '<div class="clearfix"></div>' .
+                        '<div class="file-preview-status text-center text-success"></div>' .
+                        '<div class="kv-fileinput-error"></div>' .
+                        '</div>'
+                    ]),
+                ],
+            ]
+        );
+        $this->pluginEvents['fileselect'] = "function(event, numFiles, label) {
+            $(this).fileinput('upload');
+        }";
+        $this->pluginEvents['filepreupload'] = "function (event, data, previewId, index) {
+            var file = data.files[0],
+                extension = file.name.substr((~-file.name.lastIndexOf('.') >>> 0) + 1),
+                fileName = data.form.get('key');
+            data.form.delete(this.name);
+            data.form.delete('file_id');
+            data.form.delete('key');
+            data.form.set('content-type', file.type);
+            data.form.set('key', fileName + extension);
+            data.form.set('file', file);
+        }";
+        $this->pluginEvents['fileuploaded'] = "function (event, data, index) {
+            var file = data.files[0],
+                extension = file.name.substr((~-file.name.lastIndexOf('.') >>> 0) + 1),
+                form = $('form#" . $this->field->form->id . "'),
+                filePath = '" . $awsPostObject->getFormAttributes()['action'] . "/' + data.extra.key + extension;
+            form.find('input[type=hidden][name=\"' + this.name + '\"]').val(filePath);
+            form.find('.file-caption-name').attr('title', '').val(filePath);
+        }";
+        $this->pluginEvents['filecleared'] = "function (event, id, index) {
+            $('form#" . $this->field->form->id . "').find('input[type=hidden][name=\"' + this.name + '\"]').val('');
+        }";
+        $this->getView()->registerJs("$('form#" . $this->field->form->id ."').on('beforeSubmit', function () {
+            if ($(this).find('.has-error').length) {
+                return false;
+            }
+            return true;
+        });");
+        parent::init();
+    }
+
+    /**
+     * Returns unique filename
+     * @return string
+     * @throws InvalidConfigException
+     */
+    protected function getUniqueFileName()
+    {
+        $formName = Inflector::camel2id($this->model->formName());
+        if ($this->fileNameParts) {
+            return $filename = $formName . '-' . join('-', $this->fileNameParts) . '-' . uniqid();
+        }
+        return join('-', [$formName, $this->model->{$this->uniqueKey}, $this->field->attribute, uniqid()]);
+    }
+}
